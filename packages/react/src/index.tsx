579a17d26220c912d661eaa2b509b03d3eb87e62
Lucas Goncalves <lucas.gabriel.goncalves12@gmail.com>
2023-07-26 22:26:09 -0300

    feat: created button stories

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,13 +1,2 @@
-import { styled } from './styles'
-
-const Button = styled('button', {
-  fontFamily: '$default',
-  background: '$ignite500',
-  borderRadius: '$md',
-  height: '$10',
-  padding: '$4',
-})
-
-export function App() {
-  return <Button>Hello world!</Button>
-}
+export { Button } from './components/Button'
+export type { ButtonProps } from './components/Button'
